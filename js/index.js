b81d3fc96dcf60371dc8f9984a927bbce79cb14b
chefboyrdeuce <jpldeuce@gmail.com>
2019-07-31 21:20:13 -0400

    add animation quote and styling

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    var quote = document.querySelector('.sliding-text')
+    quote.style.marginLeft = "25px";
+  };
+  
+
+//   window.onload = function() {
+//     var ball = document.getElementById('ball')
+//     ball.style.marginLeft = "25px";
+//   };
+  
